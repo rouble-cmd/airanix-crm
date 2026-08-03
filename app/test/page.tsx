edcda48f3214ef30367a01e0401fb51130b62e35
rouble-cmd <rouble@airanix.com>
2026-08-03 23:52:49 +0530

    Remove: test page

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function TestPage() {
-  return (
-    <div style={{ padding: '40px', textAlign: 'center' }}>
-      <h1>✅ TEST PAGE - WORKING</h1>
-      <p>If you see this, the deployment is working correctly.</p>
-      <p>The CRM app is live and deployed.</p>
-    </div>
-  )
-}
